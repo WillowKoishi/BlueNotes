f49e8f8a6a31b9e0980dc5517d59c11b582aa9d7
柳树怪 <wolliw@163.com>
2025-03-13 15:01:14 +0800

    114514

--- min_sys.py
+++ min_sys.py
@@ -5,6 +5,12 @@ window = pygame.display.set_mode((1280,720))
 pygame.display.set_caption("BlueNotes",)
 pygame.display.set_icon(pygame.image.load("assets/images/ui/icon.png").convert_alpha())
 
+window_w,window_h = window.get_size()
+
+ini_bg = pygame.image.load("assets/images/ui/icon.png").convert_alpha()
+ini_bg_w,ini_bg_h = ini_bg.get_size()
+window.blit(ini_bg,(window_w/2-ini_bg_w/2,window_h/2-ini_bg_w/2))
+pygame.display.flip()
 isRunning = True
 while isRunning:
     for event in pygame.event.get():
